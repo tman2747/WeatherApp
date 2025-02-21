8f36d07571dbec81d27de4a2ae5e0a5848bfd91a
Triston Land <tristonland@gmail.com>
2025-02-21 01:18:07 -0700

    add getting weather and updating

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import "./styles.css";
 
-const APIKEY = "ENTMZQA9EVHW2YTBWGFJYNPDQ"
+
+
+// try {
+//     const response = await fetch(
+//         `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?unitGroup=us&key=${APIKEY}&contentType=json`,
+//         { mode: "cors" },
+//     );
+//     await new Promise((resolve) => setTimeout(resolve, 2000));
+//     if (!response.ok) {
+//         throw new Error("something went wrong in the response was not ok :(");
+//     }
+//     const data = await response.json();
+//     return data;
+// } catch (error) {
+//     console.log(error);
+//     return null;
+// }
---
+++ src/weatherapp.js
@@ -0,0 +1,22 @@
+async function getweather(location) {
+	const APIKEY = "ENTMZQA9EVHW2YTBWGFJYNPDQ";
+	try {
+		const apiResponse = await fetch(
+			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?unitGroup=us&key=${APIKEY}&contentType=json`,
+			{ mode: "cors" },
+		);
+		if (!apiResponse.ok) {
+			throw new Error("api response was not okay.. :(");
+		}
+		return apiResponse.json();
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+}
+
+async function updateWeather(location) {
+	const data = await getweather(location);
+	return data
+}
+updateWeather("brighton co");
